76801dcf6463f99aa58df2250a3a7663f0006806
Sajfarsaju <sajfarmmuhammad@gmail.com>
2023-10-19 12:37:37 +0530

    firts commit

---
+++ src/pages/user/JobSinglePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../../components/company/home/Footer'
+import Navbar from '../../components/user/home/Navbar'
+import SingleJob from '../../components/user/jobs/SingleJob'
+
+function JobSinglePage() {
+  return (
+    <>
+    <Navbar/>
+    <SingleJob/>
+    <Footer/>
+    </>
+  )
+}
+
+export default JobSinglePage
---
+++ src/pages/user/JobsPage.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from 'react'
+import Navbar from '../../components/user/home/Navbar'
+import JobSection from '../../components/user/jobs/JobSection'
+import Footer from '../../components/company/home/Footer'
+
+function JobsPage() {
+  const [searchQuery, setSearchQuery] = useState('');
+  return (
+    <>
+    <Navbar searchValue={searchQuery} setSearchQuery={setSearchQuery} />
+    <JobSection searchQuery={searchQuery} />
+    <Footer/>
+    </>
+  )
+}
+
+export default JobsPage
