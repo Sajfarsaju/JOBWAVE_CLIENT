76fc5685309daa1d2408394fc1d509bddbde4a9b
Sajfarsaju <sajfarmmuhammad@gmail.com>
2023-11-15 19:43:51 +0530

    changes is context

--- src/context/UserAuthContext.jsx
+++ src/context/UserAuthContext.jsx
@@ -9,7 +9,7 @@ import { io } from 'socket.io-client';
 const userAuthContext = createContext();
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
-export const socketConnection = io(BACKEND_URL)
+export const socketConnection = io('https://api.jobwave.online')
 // export const socketConnection = io("http://localhost:4005/")
 export const WebSocketContext = createContext(socketConnection)
 export const WebSocketProvider = WebSocketContext.Provider;
